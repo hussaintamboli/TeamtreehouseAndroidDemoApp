64a21448aeb2bc0f3ec2999f1be23377298bb18e
Hussain <hussaint@agrostar.in>
2017-03-25 11:22:48 +0530

    Teamtreehouse lessons - Build a Simple Android App

---
+++ app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -0,0 +1,33 @@
+package com.teamtreehouse.funfacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class FunFactsActivity extends AppCompatActivity {
+
+    // Declare our view variables
+    private TextView mFactTextView;
+    private Button mShowFactButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fun_facts);
+
+        // Assign the views from the layout file to the corresponding variables
+        mFactTextView = (TextView) findViewById(R.id.factTextView);
+        mShowFactButton = (Button) findViewById(R.id.showFactButton);
+
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+        mShowFactButton.setOnClickListener(listener);
+
+    }
+}
